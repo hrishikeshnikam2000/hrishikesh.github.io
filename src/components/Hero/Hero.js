984a982aef89742b05abed622e3099fa0df706d5
Hrishikesh Nikam <hrishinikam2000@gmail.com>
2021-07-23 22:08:28 +0530

    AddHero Section Basic 👍

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,22 @@ import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
   <div>
-    Hero
+    <section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          {/* i know it's a bit much but bear with me. */}
+        fancy Seeing you here! <br />
+        Welcome to My Personal Portfolio!
+
+        </SectionTitle>
+        <SectionText>
+          {/* Descripton */}
+          The pourpose of this website is Basically to let you guys know me better.
+        </SectionText>
+        {/* Call to action button */}
+        <button onClick={() => window.location = 'mailto:hrishinikam2000@gmail.com'} >Learn More</button>
+      </LeftSection>
+    </section>
   </div>
 );
 
