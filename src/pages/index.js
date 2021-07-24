6404519fa0c1258047353ae3739c731635ed60c3
Hrishikesh Nikam <58680992+hrishikeshnikam2000@users.noreply.github.com>
2021-07-25 00:00:08 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,25 @@ import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 
+
+
+// const Home = () => {
+//   return (
+//     <Layout>
+//       {/* <Section grid> */}
+//         <Hero />
+//         {/* <BgAnimation /> */}
+//       {/* </Section> */}
+//       <Projects />
+//       <Technologies />
+//       <Timeline />
+//       <Acomplishments />
+//     </Layout>
+//   );
+// };
+
+// export default Home;
+
 const Home = () => {
   return (
     <Layout>
